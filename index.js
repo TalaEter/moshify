c13d9ea51c1f4d2e749e71a27b1f303837415b4b
TalaEter <talaeter@gmail.com>
2021-08-24 10:28:18 +0300

    added carousel edded let to variables

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-firstName = document.getElementById("first-name");
-lastName = document.getElementById("last-name");
-email = document.getElementById("email");
-message = document.getElementById("message");
-emailError = document.getElementById("email-error");
-firstNameError = document.getElementById("first-name-error");
-lastNameError = document.getElementById("last-name-error");
-formItems = [{firstName:firstName.value,lastName:lastName.value,email:email.value, message:message.value}];
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+let emailError = document.getElementById("email-error");
+let firstNameError = document.getElementById("first-name-error");
+let lastNameError = document.getElementById("last-name-error");
+let formItems = [{firstName:firstName.value,lastName:lastName.value,email:email.value, message:message.value}];
 
 class form{
 
     constructor(){
-        formItems= JSON.parse(localStorage.getItem('formItems')) || [{firstName:firstName.value,lastName:lastName.value,email:email.value, message:message.value}];
+        formItems= JSON.parse(localStorage.getItem('formItems')) || [];
         emailError.style.visibility='hidden';
         firstNameError.style.visibility='hidden';
         lastNameError.style.visibility='hidden';
@@ -50,3 +50,41 @@ let formObj;
 window.addEventListener("load", () => {
     formObj=new form();
 });
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("reviews-card");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.marginLeft = "10px"; 
+  slides[slideIndex].style.display = "block"; 
+  slides[slideIndex].style.marginLeft = "50px"; 
+  dots[slideIndex-1].className += " active";
+}
